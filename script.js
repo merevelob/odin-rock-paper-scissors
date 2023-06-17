cd751dadb528f270a520fca9375769bc49fee71b
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-17 15:49:59 -0500

    Write function playRound

--- script.js
+++ script.js
@@ -13,4 +13,54 @@ function getComputerChoice() {
 
 // console.log(getComputerChoice());
 
-// 
+// playRound
+
+function playRound(playerSelection, computerSelection) {
+    const results = {
+        rock: 'Rock beats scissors.',
+        paper: 'Paper beats rock.',
+        scissors: 'Scissors beat paper.'
+    };
+    
+    const playerSelectionLC = playerSelection.toLowerCase();
+    const computerSelectionLC = computerSelection.toLowerCase();
+    
+    // 1. Compare both selections (lower case)
+    if (computerSelectionLC === playerSelectionLC) {
+        return 'Tie!';
+    } else {
+        // My state playerWins will only change if playerSelection beats computerSelection
+        let playerWins = false;
+        switch (computerSelectionLC) {
+            case 'rock':
+                if (playerSelectionLC === 'paper') {
+                    playerWins = true;
+                }
+                break;
+            case 'paper':
+                if (playerSelectionLC === 'scissors') {
+                    playerWins = true;
+                }
+                break;
+            case 'scissors':
+                if (playerSelectionLC === 'rock') {
+                    playerWins = true;
+                }
+                break;
+            default:
+                return 'Invalid option.'
+        }
+        // 2. Return string indicating result
+        if (playerWins) {
+            return 'You win! ' + results[playerSelectionLC];
+        } else {
+            return 'You lose! ' + results[computerSelectionLC];
+        }
+    }
+}
+
+// test
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log('computerSelection:', computerSelection);
+console.log(playRound(playerSelection, computerSelection));
