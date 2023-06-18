4f31b6c7faa71fa912e8e406b82cd3c123966ea6
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-18 12:24:23 -0500

    Write function game and refactor playRound

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 function getComputerChoice() {
     // 1. Store choices in an array
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     // 2. Get a random index
     const randomIndex = Math.floor(Math.random() * choices.length);
     // 3. Return corresponding choice
@@ -23,44 +23,86 @@ function playRound(playerSelection, computerSelection) {
     };
     
     const playerSelectionLC = playerSelection.toLowerCase();
-    const computerSelectionLC = computerSelection.toLowerCase();
     
     // 1. Compare both selections (lower case)
-    if (computerSelectionLC === playerSelectionLC) {
-        return 'Tie!';
+    if (computerSelection === playerSelectionLC) {
+        console.log('Tie!');
+        return 'tie';
     } else {
-        // My state playerWins will only change if playerSelection beats computerSelection
+        // My 'state' playerWins will only change if player beats computer
         let playerWins = false;
-        switch (computerSelectionLC) {
+        switch (playerSelectionLC) {
             case 'rock':
-                if (playerSelectionLC === 'paper') {
+                if (computerSelection === 'scissors') {
                     playerWins = true;
                 }
                 break;
             case 'paper':
-                if (playerSelectionLC === 'scissors') {
+                if (computerSelection === 'rock') {
                     playerWins = true;
                 }
                 break;
             case 'scissors':
-                if (playerSelectionLC === 'rock') {
+                if (computerSelection === 'paper') {
                     playerWins = true;
                 }
                 break;
             default:
-                return 'Invalid option.'
+                // Invalid choice ends round
+                console.log('Invalid choice. You lose!');
+                return 'computerCounter';
         }
-        // 2. Return string indicating result
+        // 2. Return results
         if (playerWins) {
-            return 'You win! ' + results[playerSelectionLC];
+            console.log('You win! ' + results[playerSelectionLC]);
+            return 'playerCounter';
         } else {
-            return 'You lose! ' + results[computerSelectionLC];
+            console.log('You lose! ' + results[computerSelection]);
+            return 'computerCounter';
         }
     }
 }
 
 // test
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log('computerSelection:', computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log('computerSelection:', computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+// tiebreaker
+
+/* function tiebreaker() {
+    
+} */
+
+
+// game
+
+function game() {
+    // Counter for both players:
+    let counter = {
+        playerCounter: 0,
+        computerCounter: 0,
+        tie: 0
+    };  
+    // 1. Each round get a choice from user via prompt, call playRound, and update counters:
+    for (let i = 1; i < 6; i++) {
+        const playerSelection = prompt(`Round ${i}: Rock, paper or scissors?`);
+        const computerSelection = getComputerChoice();
+        console.log(`Round ${i} playerSelection:`, playerSelection);
+        console.log(`Round ${i} computerSelection:`, computerSelection);
+        const result = playRound(playerSelection, computerSelection);
+        // Outcomes in result match props in obj counter:
+        counter[result] += 1;
+    }
+    // 2. Compare counters and display the winner:
+    if (counter.playerCounter > counter.computerCounter) {
+        return 'YOU WIN THE GAME!';
+    } else if (counter.playerCounter < counter.computerCounter) {
+        return 'YOU LOSE THE GAME!';
+    } else {
+        return 'TIE!'
+    }
+}
+
+console.log(game());
