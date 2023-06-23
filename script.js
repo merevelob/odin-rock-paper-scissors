f191a29c285de1c43be8012dfce0c51a933a2f68
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-06-22 21:17:19 -0500

    Add EventListeners to the buttons to play rounds

--- script.js
+++ script.js
@@ -11,28 +11,30 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
-// console.log(getComputerChoice());
-
 // playRound
 
-function playRound(playerSelection, computerSelection) {
+function playRound(event) {
+    // event.stopPropagation();
     const results = {
         rock: 'Rock beats scissors.',
         paper: 'Paper beats rock.',
         scissors: 'Scissors beat paper.'
     };
+
+    const playerSelection = event.target.id;
+    console.log(playerSelection);
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
     
-    const playerSelectionLC = playerSelection.toLowerCase();
-    
-    // 1. Compare both selections (lower case)
-    if (computerSelection === playerSelectionLC) {
+    // 1. Compare both selections
+    if (computerSelection === playerSelection) {
         console.log('Tie!');
         roundDivider();
         return 'tie';
     } else {
         // My 'state' playerWins will only change if player beats computer
         let playerWins = false;
-        switch (playerSelectionLC) {
+        switch (playerSelection) {
             case 'rock':
                 if (computerSelection === 'scissors') {
                     playerWins = true;
@@ -47,16 +49,10 @@ function playRound(playerSelection, computerSelection) {
                 if (computerSelection === 'paper') {
                     playerWins = true;
                 }
-                break;
-            default:
-                // Invalid choice ends round
-                console.log('Invalid choice. You lose!');
-                roundDivider();
-                return 'computerCounter';
         }
         // 2. Return results
         if (playerWins) {
-            console.log('You win! ' + results[playerSelectionLC]);
+            console.log('You win! ' + results[playerSelection]);
             roundDivider();
             return 'playerCounter';
         } else {
@@ -67,52 +63,46 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// test
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log('computerSelection:', computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
-
 // game
 
-function game() {
-    // Counter for both players:
-    let counter = {
-        playerCounter: 0,
-        computerCounter: 0,
-        tie: 0
-    };  
-    console.log(`||| ROCK \u{270A} PAPER \u{270B} SCISSORS \u{270C} |||`);
-    // 1. Each round get a choice from user via prompt, call playRound, and update counters:
-    for (let i = 1; i < 6; i++) {
-        const playerSelection = prompt(`Round ${i}: Rock, paper or scissors?`);
-        const computerSelection = getComputerChoice();
-        console.log(`Round ${i} playerSelection:`, playerSelection);
-        console.log(`Round ${i} computerSelection:`, computerSelection);
-        const result = playRound(playerSelection, computerSelection);
-        // Outcomes in result match props in obj counter:
-        counter[result] += 1;
-    }
-    // 2. Display final score:
-    console.log(`-- FINAL SCORE: PLAYER ${counter.playerCounter} - COMPUTER ${counter.computerCounter}`);
+// function game() {
+//     // Counter for both players:
+//     let counter = {
+//         playerCounter: 0,
+//         computerCounter: 0,
+//         tie: 0
+//     };  
+//     console.log(`||| ROCK \u{270A} PAPER \u{270B} SCISSORS \u{270C} |||`);
+//     // 1. Each round get a choice from user via prompt, call playRound, and update counters:
+//     for (let i = 1; i < 6; i++) {
+//         const playerSelection = prompt(`Round ${i}: Rock, paper or scissors?`);
+//         const computerSelection = getComputerChoice();
+//         console.log(`Round ${i} playerSelection:`, playerSelection);
+//         console.log(`Round ${i} computerSelection:`, computerSelection);
+//         const result = playRound(playerSelection, computerSelection);
+//         // Outcomes in result match props in obj counter:
+//         counter[result] += 1;
+//     }
+//     // 2. Display final score:
+//     console.log(`-- FINAL SCORE: PLAYER ${counter.playerCounter} - COMPUTER ${counter.computerCounter}`);
     
-    // 2. Compare counters and display the winner:
-    // tiebreaker (returns either 0 or 1) to add 1 more ocurrence randomly
-    if (counter.playerCounter === counter.computerCounter) {
-        console.log('-- TIE! Winner will be decided randomly.');
-        if(tiebreaker()) {
-            counter.playerCounter += 1;
-        } else {
-            counter.computerCounter += 1;
-        }
-    }
-    // Winner:
-    if (counter.playerCounter > counter.computerCounter) {
-        return '** YOU WIN THE GAME! **';
-    } else {
-        return '** YOU LOSE THE GAME! **';
-    }
-}
+//     // 2. Compare counters and display the winner:
+//     // tiebreaker (returns either 0 or 1) to add 1 more ocurrence randomly
+//     if (counter.playerCounter === counter.computerCounter) {
+//         console.log('-- TIE! Winner will be decided randomly.');
+//         if(tiebreaker()) {
+//             counter.playerCounter += 1;
+//         } else {
+//             counter.computerCounter += 1;
+//         }
+//     }
+//     // Winner:
+//     if (counter.playerCounter > counter.computerCounter) {
+//         return '** YOU WIN THE GAME! **';
+//     } else {
+//         return '** YOU LOSE THE GAME! **';
+//     }
+// }
 
 // console.log(game());
 
@@ -128,3 +118,8 @@ function tiebreaker() {
     // Returns either 0 or 1
     return Math.floor(Math.random() * 2);
 }
+
+// Event listeners
+
+const choices = document.querySelectorAll('.choice > img');
+choices.forEach(choice => choice.addEventListener('click', playRound));
